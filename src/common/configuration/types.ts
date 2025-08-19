a377e9225b7a34f37db027f4ef98421c8758c512
Paul Horton <phorton@sonatype.com>
2025-08-19 08:29:09 +0100

    formtatting
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- src/common/configuration/types.ts
+++ src/common/configuration/types.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { ApiApplicationDTO } from "@sonatype/nexus-iq-api-client"
 import { LogLevel } from "../logger"
 
