9d0e7b00925377ca0ef44aea0273c4e7d8d03d52
Paul Horton <phorton@sonatype.com>
2025-09-16 14:52:53 +0100

    fix: Edge Webstore publishing (#220)
    * fix: missing dependency to run Edge Webstore publish
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    * fix: Move to using new Publish API for Microsoft Edge Webstore
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>
    
    ---------
    
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- scripts/edge-webstore.js
+++ scripts/edge-webstore.js
@@ -14,79 +14,71 @@
  * limitations under the License.
  */
 
-const fs = require('fs')
-const ews = '@plasmo-corp/ewu'
+const { EdgeAddonsAPI } = require('@plasmohq/edge-addons-api');
+const { existsSync } = require('fs');
+const process = require('process');
 
 if (process.argv.length < 3) {
-    console.error(`Expected a two arguments: ${process.argv[1]} <verify|publish> <major|minor|patch>`)
-    process.exit(1)
+    console.error(`Expected a two arguments: ${process.argv[1]} <verify|publish> <major|minor|patch>`);
+    process.exit(1);
 }
 
-const SCRIPT_MODE = process.argv[2].toUpperCase()
-let RELEASE_TYPE = 'UNKNOWN'
+const SCRIPT_MODE = process.argv[2].toUpperCase();
+let RELEASE_TYPE = 'UNKNOWN';
 if (SCRIPT_MODE === 'PUBLISH') {
-    RELEASE_TYPE = process.argv[3].toUpperCase()
+    RELEASE_TYPE = process.argv[3].toUpperCase();
 }
 
-const PRODUCT_ID = process.env.EDGE_PRODUCT_ID
-const CLIENT_ID = process.env.EDGE_CLIENT_ID
-const CLIENT_SECRET = process.env.EDGE_CLIENT_SECRET
-const ACCESS_TOKEN_URL = process.env.EDGE_ACCESS_TOKEN_URL
-const SUBMISSION_NOTES = process.env.EDGE_SUBMISSION_NOTES
-const ZIP_PATH = './sonatype-platform-browser-extension.zip'
+const PRODUCT_ID = process.env.EDGE_PRODUCT_ID;
+const CLIENT_ID = process.env.EDGE_CLIENT_ID;
+const API_KEY = process.env.EDGE_API_KEY;
+const SUBMISSION_NOTES = process.env.EDGE_SUBMISSION_NOTES;
+const ZIP_PATH = './sonatype-platform-browser-extension.zip';
 
 if (RELEASE_TYPE === 'PATCH') {
-    console.log('Skipping release to Microsoft Edge Webstore for patch release')
-    process.exit(0)
+    console.log('Skipping release to Microsoft Edge Webstore for patch release');
+    process.exit(0);
 }
 
 if (PRODUCT_ID === undefined) {
-    console.log('EDGE_PRODUCT_ID is not set - cannot continue.')
-    process.exit(1)
+    console.log('EDGE_PRODUCT_ID is not set - cannot continue.');
+    process.exit(1);
 }
 
 if (CLIENT_ID === undefined) {
-    console.log('EDGE_CLIENT_ID is not set - cannot continue.')
-    process.exit(1)
+    console.log('EDGE_CLIENT_ID is not set - cannot continue.');
+    process.exit(1);
 }
 
-if (CLIENT_SECRET === undefined) {
-    console.log('EDGE_CLIENT_SECRET is not set - cannot continue.')
-    process.exit(1)
-}
-
-if (ACCESS_TOKEN_URL === undefined) {
-    console.log('EDGE_ACCESS_TOKEN_URL is not set - cannot continue.')
-    process.exit(1)
+if (API_KEY === undefined) {
+    console.log('EDGE_API_KEY is not set - cannot continue.');
+    process.exit(1);
 }
 
 if (SUBMISSION_NOTES === undefined) {
-    console.log('EDGE_SUBMISSION_NOTES is not set - cannot continue.')
-    process.exit(1)
+    console.log('EDGE_SUBMISSION_NOTES is not set - cannot continue.');
+    process.exit(1);
 }
 
-if (SCRIPT_MODE == 'PUBLISH') {
-    if (!fs.existsSync(ZIP_PATH)) {
-        console.log(`ZIP does not exist at expected location: ${ZIP_PATH}`)
-        process.exit(1)
+console.log("PRODUCT_ID = " + PRODUCT_ID);
+
+const client = new EdgeAddonsAPI({
+    productId: PRODUCT_ID,
+    clientId: CLIENT_ID,
+    apiKey: API_KEY,
+});
+
+if (SCRIPT_MODE === 'PUBLISH') {
+    if (!existsSync(ZIP_PATH)) {
+        console.log(`ZIP does not exist at expected location: ${ZIP_PATH}`);
+        process.exit(1);
     }
 
-    import(ews).then((module) => {
-        const client = new module.EdgeWebstoreClient({
-            productId: PRODUCT_ID,
-            clientId: CLIENT_ID,
-            clientSecret: CLIENT_SECRET,
-            accessTokenUrl: ACCESS_TOKEN_URL,
-        })
-
-        client
-            .submit({
-                filePath: ZIP_PATH,
-                notes: atob(SUBMISSION_NOTES),
-            })
-            .then((r) => {
-                console.log(r)
-                console.log('Published to Edge Web Store')
-            })
-    })
+    client.submit({
+        filePath: ZIP_PATH,
+        notes: atob(SUBMISSION_NOTES),
+    }).then((r) => {
+        console.log(r);
+        console.log('Published to Edge Web Store');
+    });
 }
