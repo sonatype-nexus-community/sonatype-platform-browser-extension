4019049aede8686db406bea063917efbcb0b0f21
Paul Horton <phorton@sonatype.com>
2025-10-29 17:30:24 +0000

    aligned date formatting to be browser locale aware
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- src/side-panel/components/component.tsx
+++ src/side-panel/components/component.tsx
@@ -14,7 +14,13 @@
  * limitations under the License.
  */
 import { faTimeline } from '@fortawesome/free-solid-svg-icons'
-import { NxButton, NxFontAwesomeIcon, NxInfoAlert, NxSmallThreatCounter, NxTable } from '@sonatype/react-shared-components'
+import {
+    NxButton,
+    NxFontAwesomeIcon,
+    NxInfoAlert,
+    NxSmallThreatCounter,
+    NxTable,
+} from '@sonatype/react-shared-components'
 import React from 'react'
 import { ComponentLegalUtil } from '../../common/component/component-legal-util'
 import { MATCH_STATE_EXACT } from '../../common/component/constants'
@@ -48,14 +54,14 @@ export default function Component(props: Readonly<{ component: ComponentData }>)
     return (
         <section className='nx-tile'>
             <header className='nx-tile-header'>
-                <hgroup className="nx-tile-header__headings">
+                <hgroup className='nx-tile-header__headings'>
                     <div className='nx-tile-header__title'>
                         <h3 className='nx-h3'>{props.component.componentDetails?.component?.displayName as string}</h3>
                     </div>
                 </hgroup>
-                <div className="nx-tile__actions">
+                <div className='nx-tile__actions'>
                     <NxButton onClick={viewComponentTimeline} title={ThisBrowser.i18n.getMessage('VERSION_TIMELINE')}>
-                        <NxFontAwesomeIcon icon={faTimeline}/>
+                        <NxFontAwesomeIcon icon={faTimeline} />
                         <span>{ThisBrowser.i18n.getMessage('TIMELINE')}</span>
                     </NxButton>
                 </div>
@@ -80,7 +86,14 @@ export default function Component(props: Readonly<{ component: ComponentData }>)
                                     <NxTable.Row>
                                         <NxTable.Cell>{ThisBrowser.i18n.getMessage('CATALOG_DATE')}</NxTable.Cell>
                                         <NxTable.Cell>
-                                            {formatDate(new Date(props.component.componentDetails?.catalogDate))}
+                                            {new Date(props.component.componentDetails?.catalogDate).toLocaleDateString(
+                                                ThisBrowser.i18n.getUILanguage(),
+                                                {
+                                                    month: 'short',
+                                                    day: 'numeric',
+                                                    year: 'numeric',
+                                                }
+                                            )}
                                         </NxTable.Cell>
                                     </NxTable.Row>
                                 )}
@@ -110,15 +123,17 @@ export default function Component(props: Readonly<{ component: ComponentData }>)
                 <section className='nx-tile'>
                     <div className='nx-tile-content'>
                         <NxInfoAlert>
-                            <h3 className="nx-h3">{ThisBrowser.i18n.getMessage('HEADING_NO_COMPONENTS_MATCHED')}</h3>
-                            <p className="nx-p">
+                            <h3 className='nx-h3'>{ThisBrowser.i18n.getMessage('HEADING_NO_COMPONENTS_MATCHED')}</h3>
+                            <p className='nx-p'>
                                 {ThisBrowser.i18n.getMessage('CONTENT_NO_COMPONENTS_MATCHED')}
-                                The Component with the following PackageURL is not known to Sonatype:<br />
+                                The Component with the following PackageURL is not known to Sonatype:
+                                <br />
                             </p>
                             <p className='nx-p'>
-                                This could be for one of two reasons:<br />
-
-                                1. An invalid Package URL was generated for this Component<br />
+                                This could be for one of two reasons:
+                                <br />
+                                1. An invalid Package URL was generated for this Component
+                                <br />
                                 2. This Component has not been cataloged by Sonatype
                             </p>
                             <code>{props.component.componentDetails?.component?.packageUrl}</code>
